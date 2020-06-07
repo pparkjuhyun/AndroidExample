f21c6f99000b88488aed3e761e96108adb28f529
juhyun park <qkrwn0429@gmail.com>
2020-06-07 20:21:28 +0900

    200607 databinding lv1
    DataBinding을 이용한 UI - Data 연결

--- MVVM/app/src/main/java/com/pparkjuhyun/mvvm/MainActivity.kt
+++ MVVM/app/src/main/java/com/pparkjuhyun/mvvm/MainActivity.kt
@@ -2,11 +2,17 @@ package com.pparkjuhyun.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.pparkjuhyun.mvvm.databinding.ActivityMainBinding
+import com.pparkjuhyun.mvvm.model.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.user = User("park", "juhyun")
     }
 }
---
+++ MVVM/app/src/main/java/com/pparkjuhyun/mvvm/model/User.kt
@@ -0,0 +1,3 @@
+package com.pparkjuhyun.mvvm.model
+
+data class User(val firstName: String, val lastName: String)
