fb450cfbb0b2c52f1b909352d8bfc10a0b9d9b16
juhyunpark <qkrwn0429@gmail.com>
2020-08-11 19:51:38 +0900

    200811 add description

--- RoomExampleKotlin/app/src/main/java/ppjh/mvvm/roomexamplekotlin/MainActivity.kt
+++ RoomExampleKotlin/app/src/main/java/ppjh/mvvm/roomexamplekotlin/MainActivity.kt
@@ -14,10 +14,13 @@ class MainActivity : AppCompatActivity() {
         val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "todo-db")
                 .allowMainThreadQueries()
                 .build()
+
+        //LiveData를 적용해 LiveDataList를 return받고 해당 데이터를 observe해 publish할때마다 뷰 업데이트
         db.todoDao().getAll().observe(this, Observer { todos -> result_text.text = todos.toString() })
 
         add_btn.setOnClickListener {
             db.todoDao().insert(Todo(todo_edit.text.toString()))
+            //LiveData 적용을 통해 업데이트 후 뷰 업데이트 삭제
 //            result_text.text = db.todoDao().getAll().toString()
         }
     }
