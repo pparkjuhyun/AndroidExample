6f52c3b752b302f95875022b6d853c0033737dde
juhyunpark <qkrwn0429@gmail.com>
2020-08-15 21:51:22 +0900

    200815 java databinding

--- RoomExample/app/src/main/java/ppjh/mvvm/roomexample/MainActivity.java
+++ RoomExample/app/src/main/java/ppjh/mvvm/roomexample/MainActivity.java
@@ -1,6 +1,7 @@
 package ppjh.mvvm.roomexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
@@ -14,18 +15,18 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import ppjh.mvvm.roomexample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mTodoEditText;
-    private TextView mResultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTodoEditText = findViewById(R.id.todo_edit);
-        mResultTextView = findViewById(R.id.result_text);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setLifecycleOwner(this);
 
 //        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.clas s, "todo-db")
 //                .allowMainThreadQueries()
@@ -39,19 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
         //ViewModelProviders.of() method는 deprecated. jetpack의 ViewModel dependency를 추가해 ViewModelProvider로 인스턴스를 받아야함
         MainViewModel viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(MainViewModel.class);
+        binding.setViewModel(viewModel);
 
         //일반 뷰모델의 경우
 //        MainViewModel viewModel = new ViewModelProvider(this).get(MainViewModel.class);
-
-        viewModel.getAll().observe(this, todos -> mResultTextView.setText(todos.toString()));
-
-        findViewById(R.id.add_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                db.todoDao().insert(new Todo(mTodoEditText.getText().toString()));
-//                mResultTextView.setText(db.todoDao().getAll().toString());
-                viewModel.insert(new Todo(mTodoEditText.getText().toString()));
-            }
-        });
     }
 }
--- RoomExample/app/src/main/java/ppjh/mvvm/roomexample/MainViewModel.java
+++ RoomExample/app/src/main/java/ppjh/mvvm/roomexample/MainViewModel.java
@@ -15,19 +15,22 @@ import java.util.List;
  */
 public class MainViewModel extends AndroidViewModel {
     AppDatabase db;
+    public String newTodo;
+    public LiveData<List<Todo>> todos;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
         db = Room.databaseBuilder(application, AppDatabase.class, "todo-db")
                 .allowMainThreadQueries()
                 .build();
+        todos = getAll();
     }
 
     public LiveData<List<Todo>> getAll() {
         return db.todoDao().getAll();
     }
     
-    public void insert(Todo todo) {
-        db.todoDao().insert(todo);
+    public void insert(String todo) {
+        db.todoDao().insert(new Todo(todo));
     }
 }
