d267c4d56d23dbf5ae3aaad73b951fdd55d4eaba
juhyunpark <qkrwn0429@gmail.com>
2020-10-12 21:56:51 +0900

    201012 recyclerview 생성 및 data setting

--- MaskInfoJava/app/src/main/java/ppjh/mvvm/maskinfojava/MainActivity.java
+++ MaskInfoJava/app/src/main/java/ppjh/mvvm/maskinfojava/MainActivity.java
@@ -1,8 +1,28 @@
 package ppjh.mvvm.maskinfojava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ppjh.mvvm.maskinfojava.model.Store;
+import ppjh.mvvm.maskinfojava.model.StoreInfo;
+import ppjh.mvvm.maskinfojava.repository.MaskService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +30,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvStore = findViewById(R.id.rv_store);
+        rvStore.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        final StoreAdapter adapter = new StoreAdapter();
+        rvStore.setAdapter(adapter);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(MaskService.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
+
+        MaskService service = retrofit.create(MaskService.class);
+        Call<StoreInfo> storeInfoCall = service.fetchStoreInfo();
+        storeInfoCall.enqueue(new Callback<StoreInfo>() {
+            @Override
+            public void onResponse(Call<StoreInfo> call, Response<StoreInfo> response) {
+                adapter.updateItems(response.body().getStores());
+            }
+
+            @Override
+            public void onFailure(Call<StoreInfo> call, Throwable t) {
+                Log.e("ppjh", "onFailure: " + t.getMessage());
+            }
+        });
+    }
+}
+
+class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.StoreViewHolder> {
+    private List<Store> mItems = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public StoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_store, parent, false);
+        return new StoreViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoreViewHolder holder, int position) {
+        Store store = mItems.get(position);
+        holder.tvName.setText(store.getName());
+        holder.tvAddr.setText(store.getAddr());
+        holder.tvDistance.setText("123km");
+        holder.tvRemainStat.setText(store.getRemainStat());
+        holder.tvCount.setText("100개 이상");
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    public void updateItems(List<Store> items) {
+        mItems = items;
+        notifyDataSetChanged();
+    }
+
+    static class StoreViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        TextView tvAddr;
+        TextView tvDistance;
+        TextView tvRemainStat;
+        TextView tvCount;
+
+        public StoreViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_name);
+            tvAddr = itemView.findViewById(R.id.tv_addr);
+            tvDistance = itemView.findViewById(R.id.tv_distance);
+            tvRemainStat = itemView.findViewById(R.id.tv_remain_stat);
+            tvCount = itemView.findViewById(R.id.tv_count);
+        }
     }
 }
