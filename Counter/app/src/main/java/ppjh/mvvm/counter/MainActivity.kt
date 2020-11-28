9484df09aa545a81743fbc3d7c0493ba983bc07b
juhyunpark <qkrwn0429@gmail.com>
2020-11-28 20:58:18 +0900

    201128 saveStateHandle

--- Counter/app/src/main/java/ppjh/mvvm/counter/MainActivity.kt
+++ Counter/app/src/main/java/ppjh/mvvm/counter/MainActivity.kt
@@ -4,16 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import kotlinx.android.synthetic.main.activity_main.*
 import ppjh.mvvm.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        val TAG = MainActivity::class.java.simpleName
-    }
-
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
