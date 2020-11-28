953572c713382c8880b196e3f2293bbbb277ade1
juhyunpark <qkrwn0429@gmail.com>
2020-11-28 20:50:17 +0900

    201128 databinding 활용
    데이터 바인딩을 이용한 mvvm 패턴 최종 구현

--- Counter/app/src/main/java/ppjh/mvvm/counter/MainActivity.kt
+++ Counter/app/src/main/java/ppjh/mvvm/counter/MainActivity.kt
@@ -1,12 +1,12 @@
 package ppjh.mvvm.counter
 
-import android.app.Activity
-import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
+import ppjh.mvvm.counter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,34 +14,12 @@ class MainActivity : AppCompatActivity() {
         val TAG = MainActivity::class.java.simpleName
     }
 
-    val viewModel by viewModels<MainViewModel>()
-
-
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate: ")
-        tv_counter.text = "${viewModel.count}"
-        
-        btn_add.setOnClickListener {
-            viewModel.count++
-            tv_counter.text = "${viewModel.count}"
-        }
-
-        btn_sub.setOnClickListener {
-            viewModel.count--
-            tv_counter.text = "${viewModel.count}"
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", viewModel.count)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        viewModel.count = savedInstanceState.getInt("count")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
--- Counter/app/src/main/java/ppjh/mvvm/counter/MainViewModel.kt
+++ Counter/app/src/main/java/ppjh/mvvm/counter/MainViewModel.kt
@@ -1,7 +1,20 @@
 package ppjh.mvvm.counter
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    var count = 0
+    private var count = 0
+        set(value) {
+            field = value
+            countLiveData.value = value
+        }
+    val countLiveData = MutableLiveData<Int>()
+
+    fun increase() {
+        count++
+    }
+    fun decrease() {
+        count--
+    }
 }
